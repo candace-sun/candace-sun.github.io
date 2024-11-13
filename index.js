6285c57d7140aec40146dd2a37f3c32ae9b9f66b
Candace <csun0@terpmail.umd.edu>
2024-11-13 15:42:18 -0500

    edits cont

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-let skills = 
-`{
-  languages: ['Python', 'JavaScript', 'HTML', 'CSS', 'Java', 'C', 'C++', 'SQL', 'R', 'Rust', 'OCaml'],
-  tech: ['React', 'Firebase', 'Node.js', 'Express', 'MongoDB', 'PostgreSQL', 'Flask', 'AWS'],
-  interests: ['full-stack development', 'design and UI/UX', 'cloud computing', 'learning!']
+let skills = `{
+  <b>languages</b>: ['Python', 'JavaScript', 'HTML', 'CSS', 'Java', 'C', 'C++', 'SQL', 'R', 'Rust', 'OCaml, 'TypeScript'],
+  <b>tech:</b> ['React & React Native', 'Firebase', 'Node.js', 'MongoDB', 'Flask', 'AWS', 'Express', 'Tailwind', 'PostgreSQL', 'Docker'],
+  <b>interests:</b> ['full-stack development', 'design and UI/UX', 'cloud computing', 'learning!']
 }`;
 
-let interests = 
-`I also enjoy:<br>
+let interests = `I also enjoy:<br>
 • Hiking<br>
 • Music, and curating playlists <br>
 • Jewelry making<br>
@@ -16,8 +14,7 @@ let interests =
 • Watching TED Talks!<br>`;
 
 document.getElementById("skills-list").innerHTML = skills;
-document.getElementById("interests").innerHTML = interests; 
- 
+document.getElementById("interests").innerHTML = interests;
 
 // The place I saw this effect:
 // https://angle2.agency/
@@ -25,24 +22,24 @@ document.getElementById("interests").innerHTML = interests;
 // Red Staper made a NICE tutorial about this effect! You should check it out!
 // https://www.youtube.com/watch?v=LgiadQQM6mo&t=5s
 
-window.addEventListener('mousemove', handleMouseMove);
-window.addEventListener('resize', handleWindowResize);
+window.addEventListener("mousemove", handleMouseMove);
+window.addEventListener("resize", handleWindowResize);
 
-const spansSlow = document.querySelectorAll('.spanSlow');
-const spansFast = document.querySelectorAll('.spanFast');
+const spansSlow = document.querySelectorAll(".spanSlow");
+const spansFast = document.querySelectorAll(".spanFast");
 
 let width = window.innerWidth;
 
 function handleMouseMove(e) {
-  let normalizedPosition = e.pageX / (width/2) - 1;
+  let normalizedPosition = e.pageX / (width / 2) - 1;
   let speedSlow = 100 * normalizedPosition;
   let speedFast = 200 * normalizedPosition;
   spansSlow.forEach((span) => {
     span.style.transform = `translate(${speedSlow}px)`;
   });
   spansFast.forEach((span) => {
-    span.style.transform = `translate(${speedFast}px)`
-  })
+    span.style.transform = `translate(${speedFast}px)`;
+  });
 }
 //we need to recalculate width when the window is resized
 function handleWindowResize() {
